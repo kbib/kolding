cd4a6c23b2385f1a22755d390755a501ff42abc7
troelslenda <troels+git@reload.dk>
2011-05-24 10:51:42 +0200

    styling login area + additional page adjustments.

--- template.php
+++ template.php
@@ -12,3 +12,14 @@ function kolding_preprocess(&$variables){
 
 $variables['rsslink'] = l(theme('image',path_to_theme('kolding').'/images/rssknap.png'),'<front>',array('html' => true, 'attributes' => array('class' => 'rsslink	')));
 }
+function kolding_user_login_block($form){
+/*  $form['submit']['#type'] = "submit" ;
+
+  $name = drupal_render($form['name']);
+  $pass = drupal_render($form['pass']);
+  $submit = drupal_render($form['submit']);
+  $remember = drupal_render($form['remember_me']);
+
+  $before = '<h3>'.t('Login &rsaquo;').'</h3>';//.l(t('> NemLog-in'),'/nemlogin',array('attributes' => array('class' => 'nemlogin')));
+  return $before . $name . '<div>' . $pass . $submit . '</div>' . $remember . drupal_render($form);*/
+}
