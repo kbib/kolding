716e001f6e1d357074c812f226dd24589902e40a
Kasper Garnæs <kasper@reload.dk>
2011-06-22 13:59:15 +0200

    Update theming to support sliding doors for ding boxes

--- template.php
+++ template.php
@@ -32,3 +32,72 @@ function kolding_user_login_block($form){
   $before = '<h3>'.t('Login &rsaquo;').'</h3>';//.l(t('> NemLog-in'),'/nemlogin',array('attributes' => array('class' => 'nemlogin')));
   return $before . $name . '<div>' . $pass . $submit . '</div>' . $remember . drupal_render($form);*/
 }
+
+/**
+ * Theming panels panes. Overrides Dynamo to add additional wrappers to support sliding doors.
+ */
+function kolding_panels_pane($content, $pane, $display) {
+  if (!empty($content->content)) {
+    $idstr = $classstr = '';
+    if (!empty($content->css_id)) {
+      $idstr = ' id="' . $content->css_id . '"';
+    }
+    if (!empty($content->css_class)) {
+      $classstr = ' ' . $content->css_class;
+    }
+
+    $output = "<div class=\"panel-pane pane-$pane->subtype $classstr \"$idstr>\n";
+
+    if (!empty($content->title)) {
+      // Added title wrapper to support sliding doors in both directions
+      $output .= '<div class="pane-title">'."\n";
+      
+      if($pane->subtype == "event_list-panel_pane_1"  OR $pane->subtype == "recommendation_list"){
+        $output .= "<h1>$content->title</h1>\n";
+      }
+      elseif(
+        $pane->subtype == "topic_list" OR
+        $pane->subtype == "event_list" OR
+        $pane->subtype == "library_feature_detail_list" OR
+        $pane->subtype == "node_content"
+      ) {
+        $output .= '<h2 class="panel-title">'. $content->title .'</h2>';
+      }
+      else {
+        $output .= "<h3>$content->title</h3>\n";
+      }
+      
+      // End title wrapper
+      $output .= '</div>'."\n";
+    }
+    
+    // Added content wrapper to support sliding doors in both directions
+    $output .= '<div class="pane-content">'."\n";
+    
+    if (!empty($content->feeds)) {
+      $output .= "<div class=\"feed\">" . implode(' ', $content->feeds) . "</div>\n";
+    }
+
+    $output .= $content->content;
+
+    if (!empty($content->links)) {
+      $output .= "<div class=\"links\">" . theme('links', $content->links) . "</div>\n";
+    }
+
+    if (!empty($content->more)) {
+      if (empty($content->more['title'])) {
+        $content->more['title'] = t('more');
+      }
+      $output .= "<div class=\"panels more-link\">" . l($content->more['title'], $content->more['href']) . "</div>\n";
+    }
+    if (user_access('view pane admin links') && !empty($content->admin_links)) {
+      $output .= "<div class=\"admin-links panel-hide\">" . theme('links', $content->admin_links) . "</div>\n";
+    }
+
+    // End content wrapper
+    $output .= '</div>'."\n";
+    
+    $output .= "</div>\n";
+    return $output;
+  }
+}
