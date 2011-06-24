83d408feaff23a065ce1cc3b65b2d193b255d414
troelslenda <troels+git@reload.dk>
2011-06-24 14:37:00 +0200

    theming

--- js/setup.js
+++ js/setup.js
@@ -25,6 +25,25 @@ $(function () {
 
   */
   
+	if($(window).width() < (1281)){
+		$('#page-inner').css({'margin':'0','left':'0'});
+	}else if($(window).width() > (1500)){
+		$('#page-inner').css({'left':'0'});
+	}
+	else{
+		$('#page-inner').css({'margin':'0 auto','left':'-120px'});
+	}
   
   
+});
+
+$(window).resize(function(){
+	if($(window).width() < (1281)){
+		$('#page-inner').css({'margin':'0','left':'0'});
+	}else if($(window).width() > (1500)){
+		$('#page-inner').css({'left':'0'});
+	}
+	else{
+		$('#page-inner').css({'margin':'0 auto','left':'-120px'});
+	}
 });
