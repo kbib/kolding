ce4f16a1d5db5da89d9f05e56d5f7e3fea4445e7
Mikkel Hoegh <mikkel@hoegh.org>
2011-06-25 21:05:17 +0200

    Removing unused code.

--- js/kolding.js
+++ js/kolding.js
@@ -5,26 +5,6 @@ jQuery(function($) {
 	  'width' : '180',
   });
 
-
-/*	$('.view-id-Ingredients').ajaxComplete(function() {
-		$('#edit-tid option:eq(0)').html('Alle');
-	//IE has trouble if we try to set at width, so don't
-	var attr = (jQuery.browser.msie) ? {} : {width:'253px'};
-		$('.views-exposed-widget #edit-tid').selectmenu(attr);
-	});
-
-	$('#edit-tid option:eq(0)').html('Alle');
-	//IE has trouble if we try to set at width, so don't
-	  var attr = (jQuery.browser.msie) ? {} : {width:'253px'};
-	$('.views-exposed-widget #edit-tid').selectmenu(attr);
-
-	$('.view-id-Ingredients').ajaxStart(function(){
-		$('.views-view-grid').css({'opacity':'0.2','position':'relative'}).after('<div class="ajax_loader">&nbsp;</div>');
-
-	});
-
-  */
-
 	if($(window).width() < (1281)){
 		$('#page-inner').css({'margin':'0','left':'0'});
 	}else if($(window).width() > (1500)){
