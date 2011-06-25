4c2014bfdbe1e0574bee2a8fb1a67de3ab845599
Mikkel Hoegh <mikkel@hoegh.org>
2011-06-25 19:07:43 +0200

    Not putting RSS link in all templates.
    Removed unused code.

--- template.php
+++ template.php
@@ -16,21 +16,11 @@ function kolding_ting_search_form($form){
 	return drupal_render($form);	
 }
 
-function kolding_preprocess(&$variables){
-
-  //drupal_add_js(drupal_get_path('theme', 'kolding').'/js/selectmenu.js');
-  $variables['rsslink'] = l(theme('image',path_to_theme('kolding').'/images/rssknap.png'),'<front>',array('html' => true, 'attributes' => array('class' => 'rsslink	')));
-}
-function kolding_user_login_block($form){
-/*  $form['submit']['#type'] = "submit" ;
-
-  $name = drupal_render($form['name']);
-  $pass = drupal_render($form['pass']);
-  $submit = drupal_render($form['submit']);
-  $remember = drupal_render($form['remember_me']);
-
-  $before = '<h3>'.t('Login &rsaquo;').'</h3>';//.l(t('> NemLog-in'),'/nemlogin',array('attributes' => array('class' => 'nemlogin')));
-  return $before . $name . '<div>' . $pass . $submit . '</div>' . $remember . drupal_render($form);*/
+/**
+ * Preprocess variables for the page template.
+ */
+function kolding_preprocess_page(&$variables){
+  $variables['rsslink'] = l(theme('image',path_to_theme('kolding').'/images/rssknap.png'),'<front>', array('html' => true, 'attributes' => array('class' => 'rsslink	')));
 }
 
 /**
