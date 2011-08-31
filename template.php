3b734dd426ce476f69e687808a6bd1fafa0ee4eb
Kasper Garnæs <kasper@reload.dk>
2011-08-31 13:24:15 +0200

    #3313: Use correct image for login button

--- template.php
+++ template.php
@@ -15,6 +15,22 @@ function kolding_ting_search_form($form){
 	return drupal_render($form);
 }
 
+/**
+ * Override theme_user_login_block().
+ */
+function kolding_user_login_block($form){
+  // We use a different image for the login button.
+  // The rest is same as dynamo_user_login_block().
+  $form['submit']['#type'] = "image_button" ;
+  $form['submit']['#src'] = drupal_get_path('theme','kolding')."/images/logindknap.png";
+  $form['submit']['#attributes']['class'] = '';
+
+  $name = drupal_render($form['name']);
+  $pass = drupal_render($form['pass']);
+  $submit = drupal_render($form['submit']);
+  $remember = drupal_render($form['remember_me']);
+
+  return  $name . $pass . $submit . $remember . drupal_render($form);
 }
 
 /**
